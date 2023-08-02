47c7b6415b774bea0792f8d9e23f73d814be816c
bluezealot <qin_xizhou@hoperun.com>
2023-08-02 22:43:58 +0800

    Add first version.

---
+++ src/main/java/bluezealot/translateppt/TranslatepptApplication.java
@@ -0,0 +1,13 @@
+package bluezealot.translateppt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslatepptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranslatepptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bluezealot/translateppt/TranslatepptApplicationTests.java
@@ -0,0 +1,13 @@
+package bluezealot.translateppt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslatepptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
